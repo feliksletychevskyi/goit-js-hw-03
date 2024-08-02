e7ee75a4570e622e8880f6286d5928c85e08ea65
Feliks <letych53@gmail.com>
2024-08-02 17:52:04 +0100

    first

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+    const totalArray = [];
+    for (let i = 0; i > value; i++) {
+        if (numbers > value) {
+            return totalArray.slice(-1);
+        }
+        return totalArray; 
+    }
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
